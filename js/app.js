61892bf8ef978a62ad6eb155bca510ea65fa61b7
MD RAKIBUL ISLAM RAKIB <rakibulinux@gmail.com>
2022-08-23 00:24:26 +0600

    calculator done

--- js/app.js
+++ js/app.js
@@ -1,19 +1,15 @@
-// select-1
-// titel-text-3
-// add-selected-player
-
-function getElementTextById(elementId) {
-    const elementFieldId = document.getElementById(elementId);
+function getElementTextById(elementId, buttonId) {
+    const elementFieldId = document.getElementById(elementId, buttonId);
     const elementField = elementFieldId.innerText;
     return elementField;
 }
 
-function setElementTextById(elementId) {
+function setElementTextById(elementId, buttonId) {
     let list = document.querySelector(".player-items"),
         listItems = list.getElementsByTagName("li"),
         itemsLength = listItems.length;
         if (itemsLength >= 5) {
-            alert("You can't select more then 5 players")
+            alert("You can't select more then 5 players");
         } else {
             const titelText = getElementTextById(elementId);
             const getCurrentText = document.getElementById('add-selected-player');
@@ -21,39 +17,30 @@ function setElementTextById(elementId) {
             
             createNewList.innerText = titelText;
             getCurrentText.appendChild(createNewList);
+            const disableBtnID = document.getElementById(buttonId);
+            disableBtnID.setAttribute('disabled', '');
+            disableBtnID.style.backgroundColor = 'gray';
+            disableBtnID.style.color = 'white';
         }
 }
 
-function disableBtn(buttonId){
-    const disableBtnID = document.getElementById(buttonId);
-    disableBtnID.setAttribute('disabled', '');
-    disableBtnID.style.backgroundColor = 'gray';
-    disableBtnID.style.color = 'white';
-}
-
 document.getElementById('select-1').addEventListener('click', function () {
-    setElementTextById('titel-text-1');
-    disableBtn('select-1');
-    
+    setElementTextById('titel-text-1', 'select-1');
 });
 
 document.getElementById('select-2').addEventListener('click', function () {
-    setElementTextById('titel-text-2')
-    disableBtn('select-2');
+    setElementTextById('titel-text-2', 'select-2')
 });
 document.getElementById('select-3').addEventListener('click', function () {
-    setElementTextById('titel-text-3')
-    disableBtn('select-3');
+    setElementTextById('titel-text-3', 'select-3')
 });
 document.getElementById('select-4').addEventListener('click', function () {
-    setElementTextById('titel-text-4')
-    disableBtn('select-4');
+    setElementTextById('titel-text-4', 'select-4')
 });
 document.getElementById('select-5').addEventListener('click', function () {
-    setElementTextById('titel-text-5')
-    disableBtn('select-5');
+    setElementTextById('titel-text-5', 'select-5')
 });
 document.getElementById('select-6').addEventListener('click', function () {
-    setElementTextById('titel-text-6')
-    disableBtn('select-6');
+    setElementTextById('titel-text-6', 'select-6')
+
 });
